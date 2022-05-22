50d8dfaad19ba5e2b13c477cc6539956d020154a
vivek683 <73192110+vivek683@users.noreply.github.com>
2022-05-22 19:21:58 +0530

    Add files via upload

---
+++ src/data/user.js
@@ -0,0 +1,19 @@
+var user=[{
+    "id":1,
+    "uid":"aiswaryawalter@gmail.com",
+    "pwd":"aiswarya"
+},
+{
+    "id":2,
+    "uid":"surya@gmail.com",
+    "pwd":"surya"
+},
+{
+    "id":3,
+    "uid":"bazith@gmail.com",
+    "pwd":"bazith"
+}
+]
+
+
+module.exports=user;
